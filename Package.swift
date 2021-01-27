7ef034b48b8f3e4e21db7957bb98ec6a94d4acbe
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-01-27 22:20:28 +0100

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -4,25 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "monki-map-ios-views",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "monki-map-ios-views",
-            targets: ["monki-map-ios-views"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "monki-map-ios-views",
-            dependencies: []),
-        .testTarget(
-            name: "monki-map-ios-viewsTests",
-            dependencies: ["monki-map-ios-views"]),
-    ]
+	name: "monki-map-ios-views",
+	products: [
+		.library(
+			name: "MonkiMapViews",
+			targets: ["MonkiMapViews"]
+		),
+	],
+	dependencies: [
+		.package(url: "https://github.com/MonkiProjects/monki-projects-model.git", .branch("master")),
+		.package(url: "https://github.com/MonkiProjects/monki-map-model.git", .branch("master")),
+	],
+	targets: [
+		.target(
+			name: "MonkiMapViews",
+			dependencies: [
+				.product(name: "MonkiProjectsModel", package: "monki-projects-model"),
+				.product(name: "MonkiMapModel", package: "monki-map-model"),
+			]
+		),
+		.testTarget(
+			name: "MonkiMapViewsTests",
+			dependencies: ["MonkiMapViews"]
+		),
+	]
 )
